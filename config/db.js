851ec1933555a50a8ae9593b24f0785f6f9387b7
Gaurav zore <149921595+Zoregaurav@users.noreply.github.com>
2026-04-17 23:33:19 +0530

    Refactor database connection to use environment variables

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mysql=require("mysql2");
 require('dotenv').config();
 
-const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:process.env.DB_PASSWORD,
-    database:"schoolDB",
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306
 });
-
 
 db.connect((err)=>{
     if(err) throw err;
